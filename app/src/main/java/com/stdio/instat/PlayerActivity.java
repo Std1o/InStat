e4a9a711b3bddb0d26fb46b7975bc4dc7598f5ab
Std1o <37378410+Std1o@users.noreply.github.com>
2021-07-13 09:54:19 +0300

    configuration change listener

--- app/src/main/java/com/stdio/instat/PlayerActivity.java
+++ app/src/main/java/com/stdio/instat/PlayerActivity.java
@@ -2,6 +2,7 @@ package com.stdio.instat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -31,6 +32,28 @@ public class PlayerActivity extends AppCompatActivity {
         LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) binding.videoView.getLayoutParams();
         params.width = metrics.widthPixels;// max width
         params.height = (int) (params.width * 0.5625);//ratio of height to width is approximately like 720/1280
+        binding.videoView.setLayoutParams(params);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        int newOrientation = newConfig.orientation;
+
+        if (newOrientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setFullScreenVideoParams();
+        } else {
+            setDefaultVideoParams();
+        }
+    }
+
+    private void setFullScreenVideoParams() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        android.widget.LinearLayout.LayoutParams params = (android.widget.LinearLayout.LayoutParams) binding.videoView.getLayoutParams();
+        params.width = metrics.widthPixels;
+        params.height = metrics.heightPixels;
         binding.videoView.setLayoutParams(params);
     }
 }
